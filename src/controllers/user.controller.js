52e183571693c52faa061ce69ff3640533bede43
Jose Andres Salazar <joseandres61@gmail.com>
2023-01-20 20:15:13 -0400

    feat: create delete and update user controller for user route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,6 +9,17 @@ const getUsers = async (request, response) => {
   }
 };
 
+const getUserById = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const data = await userCollection.findById(id);
+    response.send({ payload: data });
+  } catch (error) {
+    console.log(error);
+    response.send({ error });
+  }
+};
+
 const createUser = async (request, response) => {
   try {
     const { body } = request;
@@ -21,4 +32,36 @@ const createUser = async (request, response) => {
   }
 };
 
-module.exports = { getUsers, createUser };
+const updateUser = async (request, response) => {
+  try {
+    const { body } = request;
+    const { id } = request.params;
+    console.log({ id, body });
+
+    const data = await userCollection.updateOne({ _id: id }, body);
+
+    response.send({ payload: data });
+  } catch (error) {
+    console.log(error);
+    response.send({ error });
+  }
+};
+
+const deleteUser = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const { password } = request.body;
+    const user = await userCollection.findById(id);
+
+    if (password === user.password) {
+      const deletedUser = await userCollection.deleteOne({ _id: id });
+      response.send({ payload: deletedUser });
+    } else {
+      response.send({ error: 'ERROR_IN_PASSWORD' });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { getUsers, createUser, getUserById, updateUser, deleteUser };
