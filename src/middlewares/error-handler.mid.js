e923d559fc3de56b7c3b2781b7e8b8633db8ffee
Jose Andres Salazar <joseandres61@gmail.com>
2023-01-27 14:56:31 -0400

    chore: create a custom error handler middleware

---
+++ src/middlewares/error-handler.mid.js
@@ -0,0 +1,17 @@
+const ErrorHandlerMiddleware = (error, request, response, next) => {
+  console.log('Middleware Error Handling');
+
+  const errorCode = error.statusCode || 500;
+  const errorMessage = error.message || 'Something went wrong';
+
+  const json = {
+    success: false,
+    status: errorCode,
+    message: errorMessage,
+    stack: process.env.NODE_ENV === 'development' ? error.stack : {},
+  };
+
+  response.status(errorCode).json(json);
+};
+
+module.exports = { ErrorHandlerMiddleware };
