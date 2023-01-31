bef18eba1f988a97e844fc25f6ba1ed781683fea
Jose Andres Salazar <joseandres61@gmail.com>
2023-01-31 12:55:46 -0400

    feat: create validations for products endpoints

--- src/validators/products.validator.js
+++ src/validators/products.validator.js
@@ -2,23 +2,32 @@ const { body, check, param } = require('express-validator');
 const validateResults = require('../utilities/validate-results.utilitie');
 const ErrorMessages = require('../utilities/error-messages.utilitie');
 
-const getSingleProductValidator = [
+const getItemValidator = [
   param('id').exists().notEmpty().isMongoId().withMessage(ErrorMessages['mongo-id']),
   (request, response, next) => {
     validateResults(request, response, next);
   },
 ];
 
-const updateProductValidator = [
-  check('id').exists().notEmpty(),
-  body('title').isLength({ min: 5 }).optional(),
-  body('description').isLength({ min: 20 }).optional(),
-  body('price').isNumeric().optional(),
-  body('brand').isString().optional(),
-  body('category').isString().optional(),
+const createItemValidator = [
+  body('title').exists().notEmpty().isLength({ min: 5 }).withMessage(ErrorMessages['title']),
+  body('description').exists().notEmpty().isLength({ min: 20 }).withMessage(ErrorMessages['description']),
+  body('price').exists().notEmpty().isNumeric().withMessage(ErrorMessages['price']),
   (request, response, next) => {
     validateResults(request, response, next);
   },
 ];
 
-module.exports = { getSingleProductValidator, updateProductValidator };
+const updateItemValidator = [
+  check('id').exists().notEmpty().isMongoId().withMessage(ErrorMessages['mongo-id']),
+  body('title').isLength({ min: 5 }).withMessage(ErrorMessages['title']).optional(),
+  body('description').isLength({ min: 20 }).withMessage(ErrorMessages['description']).optional(),
+  body('price').isNumeric().withMessage(ErrorMessages['price']).optional(),
+  body('brand').isString().withMessage(ErrorMessages['brand']).optional(),
+  body('category').isString().isLength().withMessage(ErrorMessages['category']).optional(),
+  (request, response, next) => {
+    validateResults(request, response, next);
+  },
+];
+
+module.exports = { createItemValidator, getItemValidator, updateItemValidator };
